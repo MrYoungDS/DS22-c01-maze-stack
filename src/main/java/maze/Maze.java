5e5e783a2bc39a4982a41ad7fa152fb8925398d6
Steve Young <syoung@derryfield.org>
2022-11-01 01:13:20 -0400

    Initial commit

---
+++ src/main/java/maze/Maze.java
@@ -0,0 +1,110 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * @author Lewis and Chase
+ *
+ * Represents a maze of characters. The goal is to get from the
+ * top left corner to the bottom right, following a path of 1's.
+ */
+public class Maze
+{
+    private static final int TRIED = 3;
+    private static final int PATH = 7;
+
+    private int[][] grid = {
+            {1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1},
+            {1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1},
+            {1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1},
+            {1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1},
+            {1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1},
+            {1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
+
+    /**
+     * Marks the specified position in the maze as TRIED
+     *
+     * @param row the index of the row to try
+     * @param col the index of the column to try
+     */
+    public void tryPosition(int row, int col)
+    {
+        grid[row][col] = TRIED;
+    }
+
+    /**
+     * Return the number of rows in this maze
+     *
+     * @return the number of rows in this maze
+     */
+    public int getRows()
+    {
+        return grid.length;
+    }
+
+    /**
+     * Return the number of columns in this maze
+     *
+     * @return the number of columns in this maze
+     */
+    public int getColumns()
+    {
+        return grid[0].length;
+    }
+
+    /**
+     * Marks a given position in the maze as part of the PATH
+     *
+     * @param row the index of the row to mark as part of the PATH
+     * @param col the index of the column to mark as part of the PATH
+     */
+    public void markPath(int row, int col)
+    {
+        grid[row][col] = PATH;
+    }
+
+    /**
+     * Determines if a specific location is valid. A valid location
+     * is one that is on the grid, is not blocked, and has not been TRIED.
+     *
+     * @param row the row to be checked
+     * @param column the column to be checked
+     * @return true if the location is valid
+     */
+    public boolean validPosition(int row, int column)
+    {
+        boolean result = false;
+
+        // check if cell is in the bounds of the matrix
+        if (row >= 0 && row < grid.length &&
+                column >= 0 && column < grid[row].length)
+
+            //  check if cell is not blocked and not previously tried
+            if (grid[row][column] == 1)
+                result = true;
+
+        return result;
+    }
+
+    /**
+     * Returns the maze as a string.
+     *
+     * @return a string representation of the maze
+     */
+    public String toString()
+    {
+        String result = "\n";
+
+        for (int row=0; row < grid.length; row++)
+        {
+            for (int column=0; column < grid[row].length; column++)
+                result += grid[row][column] + "";
+            result += "\n";
+        }
+
+        return result;
+    }
+}
